3046be56a01c580eb25fa86d9a96258ff63514d8
Rehannah <rehannah12345@gmail.com>
2020-10-02 11:41:55 -0400

    Projects related tables created

---
+++ database/migrations/2020_01_02_create_contacts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->text('first_name');
+            $table->text('middle_name');
+            $table->text('last_name');
+            $table->foreignId('address_id')->constrained();
+            $table->foreignId('job_title_id')->constrained();
+            $table->timestampsTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
---
+++ database/migrations/2020_01_02_create_stakeholders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStakeholdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stakeholders', function (Blueprint $table) {
+            $table->id();
+            $table->text('first_name');
+            $table->text('middle_name');
+            $table->text('last_name');
+            $table->foreignId('address_id')->constrained();
+            $table->timestampsTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stakeholders');
+    }
+}
